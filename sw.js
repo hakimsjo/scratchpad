a38905fda0cdd14f5c81ecc8d106cb717f3a083b
hakimsjo <hakimsjo@gmail.com>
2025-09-08 21:32:55 +0200

    Add PWA support with service worker and manifest file

---
+++ sw.js
@@ -0,0 +1,52 @@
+
+const CACHE_NAME = 'scratchpad-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/style.css',
+    '/assets/js/script.js',
+    '/manifest.json',
+    '/assets/icons/icon-192x192.png',
+    '/assets/icons/icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
